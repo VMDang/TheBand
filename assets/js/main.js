810999e93dd45e8824ab910226279d35836b4ed8
VMDang <dang.vm205063@sis.hust.edu.vn>
2022-12-13 18:19:15 +0700

    menu mobile reponsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,4 +26,34 @@ modalOpen.addEventListener('click', hideBuyTicket);
 
 modalContainer.addEventListener('click', function (event) {
     event.stopPropagation();
+
+    var header = document.getElementById('header');
+    var mobileMenu = document.getElementById('mobile-menu');
+    var headerHeight = header.clientHeight;
+
+    mobileMenu.onclick = function () {
+        var isClose = header.clientHeight === headerHeight;
+        if (isClose) {
+            header.style.height = 'auto';
+        } else {
+            header.style.height = null;
+        }
+    }
+
+    var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+
+    for (let index = 0; index < menuItems.length; index++) {
+        var menuItem =  menuItems[index];
+
+        menuItem.onclick = function (event) {
+            var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+            
+            if (!isParentMenu) {
+                header.style.height = null;
+            }else {
+                event.preventDefault();
+            }
+        }
+    }
+
 });
